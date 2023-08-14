35d0d07177b783b2f5b4e810367bb8879fb70b7a
memefather <142188887+memefather@users.noreply.github.com>
2023-08-13 23:05:32 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,42 +51,42 @@ with tab2:
     openai.api_key = os.getenv("OPENAI_API_KEY")
     model_id = 'gpt-3.5-turbo'
     
-    def ChatGPT_conversation(conversation):
+    def ChatGPT_conversation(conversation2):
         response = openai.ChatCompletion.create(
             model=model_id,
-            messages=conversation
+            messages=conversation2
         )
         # api_usage = response['usage']
         # print('Total token consumed: {0}'.format(api_usage['total_tokens']))
         # stop means complete
         # print(response['choices'][0].finish_reason)
         # print(response['choices'][0].index)
-        conversation.append({'role': response.choices[0].message.role, 'content': response.choices[0].message.content})
-        return conversation
+        conversation2.append({'role': response.choices[0].message.role, 'content': response.choices[0].message.content})
+        return conversation2
     
     """
     🎼Think you are expert in realm of music? Test your knowledge!🎤
     """
     
-    def quiznow(conversation):
+    def quiznow(conversation2):
         st.subheader('\nWhat is the Next Line?\n')
-        conversation = ChatGPT_conversation(conversation)
-        st.markdown(conversation[-1]['content'].strip())
+        conversation2 = ChatGPT_conversation(conversation2)
+        st.markdown(conversation2[-1]['content'].strip())
         input = st.text_input('enter next line here', '')
         if st.button('Rock it!'):
             st.write('\n')  # add spacing
             with st.expander("GPT judge", expanded=True):
                 prompt = input
-                conversation.append({'role': 'user', 'content': prompt})
-                conversation = ChatGPT_conversation(conversation)
-                st.markdown(conversation[-1]['content'].strip())  #output the results
+                conversation2.append({'role': 'user', 'content': prompt})
+                conversation2 = ChatGPT_conversation(conversation2)
+                st.markdown(conversation2[-1]['content'].strip())  #output the results
                 if st.button('Another one!'):
                     st.experimental_rerun()
-            #print('{0}: {1}\n'.format(conversation[-1]['role'].strip(), conversation[-1]['content'].strip()))
+            #print('{0}: {1}\n'.format(conversation2[-1]['role'].strip(), conversation2[-1]['content'].strip()))
 
     if __name__ == '__main__':
         # call main function
-        conversation = []
-        conversation.append({'role': 'system', 'content': 'You are playing a game with the user. You will provide a line of lyrics from a song after this prompt and the user will guess the next line. If the answer is correct or close enough, reply "Yeah! You got it!" If incorrect, provide the right answer along with the song title, artist and year of release'})
-        quiznow(conversation)
+        conversation2 = []
+        conversation2.append({'role': 'system', 'content': 'You are playing a game with the user. You will provide a line of lyrics from a song after this prompt and the user will guess the next line. If the answer is correct or close enough, reply "Yeah! You got it!" If incorrect, provide the right answer along with the song title, artist and year of release'})
+        quiznow(conversation2)
 
